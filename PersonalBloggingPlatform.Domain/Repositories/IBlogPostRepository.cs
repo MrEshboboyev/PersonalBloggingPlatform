b83723936b6259918b95d08b65c8d372f9cd0094
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-06 16:40:03 +0500

    Repository : IBlogPostRepository

---
+++ PersonalBloggingPlatform.Domain/Repositories/IBlogPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using PersonalBloggingPlatform.Domain.Entities;
+using PersonalBloggingPlatform.Domain.ValueObjects;
+using System.Threading.Tasks;
+
+namespace PersonalBloggingPlatform.Domain.Repositories;
+
+public interface IBlogPostRepository
+{
+    Task<BlogPost> GetAsync(BlogPostId id);
+    Task CreateAsync(BlogPost blogPost);
+    Task UpdateAsync(BlogPost blogPost);
+    Task DeleteAsync(BlogPost blogPost);
+}
