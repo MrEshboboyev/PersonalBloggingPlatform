e8d7902230069688da422e1c255ac8e1ef32df67
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-06 23:03:12 +0500

    Default Configuration of EF Core : packages added, adding program.cs

--- PersonalBloggingPlatform.API/Program.cs
+++ PersonalBloggingPlatform.API/Program.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PersonalBloggingPlatform.Application;
+using PersonalBloggingPlatform.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
---
+++ PersonalBloggingPlatform.Infrastructure/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PersonalBloggingPlatform.Shared.Queries;
+
+namespace PersonalBloggingPlatform.Infrastructure;
+
+public static class Extensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddQueries();
+
+        return services;
+    }
+}
