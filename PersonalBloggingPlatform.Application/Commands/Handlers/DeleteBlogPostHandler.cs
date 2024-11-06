ebb72d422ee1e0b0609fd8b11c53fcf4a0608932
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-06 21:52:38 +0500

    DeleteBlogPost : command and handler, not found exception

---
+++ PersonalBloggingPlatform.Application/Commands/Handlers/DeleteBlogPostHandler.cs
@@ -0,0 +1,19 @@
+﻿using PersonalBloggingPlatform.Application.Exceptions;
+using PersonalBloggingPlatform.Domain.Repositories;
+using PersonalBloggingPlatform.Shared.Abstractions.Commands;
+using System.Threading.Tasks;
+
+namespace PersonalBloggingPlatform.Application.Commands.Handlers;
+
+internal sealed class DeleteBlogPostHandler(IBlogPostRepository repository) 
+    : ICommandHandler<DeleteBlogPost>
+{
+    private readonly IBlogPostRepository _repository = repository;
+
+    public async Task HandleAsync(DeleteBlogPost command)
+    {
+        var blogPost = await _repository.GetAsync(command.Id)
+            ?? throw new BlogPostNotFoundException(command.Id);
+        await _repository.DeleteAsync(blogPost);
+    }
+}
---
+++ PersonalBloggingPlatform.Application/Exceptions/BlogPostNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using PersonalBloggingPlatform.Shared.Abstractions.Exceptions;
+using System;
+
+namespace PersonalBloggingPlatform.Application.Exceptions;
+
+public class BlogPostNotFoundException(Guid id) 
+    : DomainException($"Blog Post with Id {id} not found!")
+{
+    public Guid Id { get; } = id;
+}
