9151ebc090f1f22b213ce954279bb38752eb590b
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-06 22:00:39 +0500

    Generic query and handler, following CQRS;

---
+++ PersonalBloggingPlatform.Shared.Abstractions/Queries/IQuery.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalBloggingPlatform.Shared.Abstractions.Queries;
+
+public interface IQuery
+{
+}
+
+public interface IQuery<TResult> : IQuery
+{
+}
+
---
+++ PersonalBloggingPlatform.Shared.Abstractions/Queries/IQueryHandler.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+
+namespace PersonalBloggingPlatform.Shared.Abstractions.Queries;
+
+public interface IQueryHandler<in TQuery, TResult> where TQuery : class, IQuery<TResult>
+{
+    Task<TResult> HandleAsync(TQuery query);
+}
