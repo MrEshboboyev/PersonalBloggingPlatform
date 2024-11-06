aec5f3310728c2c1e01e11f3232dd4af596fc043
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-06 23:35:38 +0500

    Read Models : BlogPostReadModel

---
+++ PersonalBloggingPlatform.Infrastructure/EF/Models/BlogPostReadModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonalBloggingPlatform.Infrastructure.EF.Models;
+
+public class BlogPostReadModel
+{
+    public Guid Id { get; set; }
+    public int Version { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
+
