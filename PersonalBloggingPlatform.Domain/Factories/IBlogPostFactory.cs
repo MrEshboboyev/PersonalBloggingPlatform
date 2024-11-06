e3c313b0e86e60d808297281416b8776cc06c501
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-06 15:42:44 +0500

    IBlogPostFactory interface

---
+++ PersonalBloggingPlatform.Domain/Factories/IBlogPostFactory.cs
@@ -0,0 +1,9 @@
+﻿using PersonalBloggingPlatform.Domain.Entities;
+using PersonalBloggingPlatform.Domain.ValueObjects;
+
+namespace PersonalBloggingPlatform.Domain.Factories;
+
+public interface IBlogPostFactory
+{
+    BlogPost CreateBlogPost(BlogPostId id, PostTitle title, PostContent content);
+}
