c7ee33a356f7196727a380bc2c0b9fe30cca9d1e
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-06 22:16:36 +0500

    IQueryDispatcher interface and implementation;

---
+++ PersonalBloggingPlatform.Shared.Abstractions/Queries/IQueryDispatcher.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+
+namespace PersonalBloggingPlatform.Shared.Abstractions.Queries;
+
+public interface IQueryDispatcher
+{
+    Task<TResult> QueryAsync<TResult>(IQuery<TResult> query);
+}
