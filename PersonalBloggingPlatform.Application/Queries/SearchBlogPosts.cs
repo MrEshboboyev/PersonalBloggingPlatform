b3ef6cbda81b249fdd4549f9851d021bc31cc861
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-06 22:49:48 +0500

    SearchBlogPosts query

---
+++ PersonalBloggingPlatform.Application/Queries/SearchBlogPosts.cs
@@ -0,0 +1,10 @@
+﻿using PersonalBloggingPlatform.Application.DTO;
+using PersonalBloggingPlatform.Shared.Abstractions.Queries;
+using System.Collections.Generic;
+
+namespace PersonalBloggingPlatform.Application.Queries;
+
+public class SearchBlogPosts : IQuery<IEnumerable<BlogPostDto>>
+{
+    public string SearchPhrase { get; set; }
+}
