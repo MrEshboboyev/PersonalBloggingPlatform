2ddcfde0be4b7a4fe79f375ff0ff712ac877002a
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-06 18:17:36 +0500

    Extensions.cs : all ICommandHandler implementations adding lifetime (with Scan) from Scrutor;

--- PersonalBloggingPlatform.Shared/Extensions.cs
+++ PersonalBloggingPlatform.Shared/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using PersonalBloggingPlatform.Shared.Abstractions.Commands;
 using PersonalBloggingPlatform.Shared.Commands;
+using System.Reflection;
 
 namespace PersonalBloggingPlatform.Shared;
 
@@ -8,7 +9,14 @@ public static class Extensions
 {
     public static IServiceCollection AddCommands(this IServiceCollection services)
     {
+        var assembly = Assembly.GetCallingAssembly();
+
         services.AddScoped<ICommandDispatcher, InMemoryCommandDispatcher>();
+
+        services.Scan(s => s.FromAssemblies(assembly)
+            .AddClasses(c => c.AssignableTo(typeof(ICommandHandler<>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
 
         return services;
     }
