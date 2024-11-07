1655c2daa990de2becce7cb0db2a6bc14643d86c
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-07 14:11:39 +0500

    BlogPost Read Service Implementation on top of EFCore

---
+++ PersonalBloggingPlatform.Infrastructure/EF/Services/PostgresBlogPostReadService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalBloggingPlatform.Application.Services;
+using PersonalBloggingPlatform.Infrastructure.EF.Contexts;
+using PersonalBloggingPlatform.Infrastructure.EF.Models;
+using System.Threading.Tasks;
+
+namespace PersonalBloggingPlatform.Infrastructure.EF.Services;
+
+internal sealed class PostgresBlogPostReadService(ReadDbContext readDbContext) : IBlogPostReadService
+{
+    private readonly DbSet<BlogPostReadModel> _blogPosts = readDbContext.BlogPosts;
+
+    public Task<bool> ExistsByTitleAsync(string title)
+    {
+        return _blogPosts.AnyAsync(bp => bp.Title == title);
+    }
+}
+
