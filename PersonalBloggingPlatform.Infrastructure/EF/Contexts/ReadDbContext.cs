a5517b2d2d69426233c62433575afbfc05527ed8
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-08 16:02:34 +0500

    DbContexts updated : entities added

--- PersonalBloggingPlatform.Infrastructure/EF/Contexts/ReadDbContext.cs
+++ PersonalBloggingPlatform.Infrastructure/EF/Contexts/ReadDbContext.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using PersonalBloggingPlatform.Infrastructure.EF.Config;
 using PersonalBloggingPlatform.Infrastructure.EF.Models;
+using PersonalBloggingPlatform.Infrastructure.EF.Config;
 
 namespace PersonalBloggingPlatform.Infrastructure.EF.Contexts;
 
 public sealed class ReadDbContext(DbContextOptions<ReadDbContext> options) : DbContext(options)
 {
     public DbSet<BlogPostReadModel> BlogPosts { get; set; }
+    public DbSet<TagReadModel> Tags { get; set; }
+    public DbSet<CategoryReadModel> Categories { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("blogPost");
 
+        // Apply Read-specific configurations
         var configuration = new ReadConfiguration();
         modelBuilder.ApplyConfiguration<BlogPostReadModel>(configuration);
         modelBuilder.ApplyConfiguration<TagReadModel>(configuration);
--- PersonalBloggingPlatform.Infrastructure/EF/Contexts/WriteDbContext.cs
+++ PersonalBloggingPlatform.Infrastructure/EF/Contexts/WriteDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PersonalBloggingPlatform.Domain.Entities;
-using PersonalBloggingPlatform.Domain.ValueObjects;
 using PersonalBloggingPlatform.Infrastructure.EF.Config;
 
 namespace PersonalBloggingPlatform.Infrastructure.EF.Contexts;
@@ -8,14 +7,19 @@ namespace PersonalBloggingPlatform.Infrastructure.EF.Contexts;
 internal sealed class WriteDbContext(DbContextOptions<WriteDbContext> options) : DbContext(options)
 {
     public DbSet<BlogPost> BlogPosts { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<Category> Categories { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("blogPost");
 
+        // Apply Write-specific configurations
         var configuration = new WriteConfiguration();
         modelBuilder.ApplyConfiguration<BlogPost>(configuration);
         modelBuilder.ApplyConfiguration<Tag>(configuration);
         modelBuilder.ApplyConfiguration<Category>(configuration);
+
+        base.OnModelCreating(modelBuilder);
     }
 }
