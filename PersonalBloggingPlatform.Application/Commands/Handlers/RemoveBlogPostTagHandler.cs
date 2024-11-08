c5a8cda5312d4c2e6253a8bfdc59561c85b48bec
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-08 10:46:51 +0500

    Tag and Category : Commands and Handlers

---
+++ PersonalBloggingPlatform.Application/Commands/Handlers/RemoveBlogPostTagHandler.cs
@@ -0,0 +1,22 @@
+﻿using PersonalBloggingPlatform.Application.Exceptions;
+using PersonalBloggingPlatform.Domain.Repositories;
+using PersonalBloggingPlatform.Domain.ValueObjects;
+using PersonalBloggingPlatform.Shared.Abstractions.Commands;
+using System.Threading.Tasks;
+
+namespace PersonalBloggingPlatform.Application.Commands.Handlers;
+
+public class RemoveBlogPostTagHandler(IBlogPostRepository repository)
+    : ICommandHandler<RemoveBlogPostTag>
+{
+    private readonly IBlogPostRepository _repository = repository;
+
+    public async Task HandleAsync(RemoveBlogPostTag command)
+    {
+        var blogPost = await _repository.GetAsync(command.BlogPostId)
+            ?? throw new BlogPostNotFoundException(command.BlogPostId);
+
+        blogPost.RemoveTag(new Tag(command.TagName));
+        await _repository.UpdateAsync(blogPost);
+    }
+}
---
+++ PersonalBloggingPlatform.Application/Commands/RemoveBlogPostTag.cs
@@ -0,0 +1,6 @@
+﻿using PersonalBloggingPlatform.Shared.Abstractions.Commands;
+using System;
+
+namespace PersonalBloggingPlatform.Application.Commands;
+
+public record RemoveBlogPostTag(Guid BlogPostId, string TagName) : ICommand;
